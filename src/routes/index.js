f3dfb8dbaf70cc8e01a2c642b3a92d09df037a13
natanborges-diniz <natanborges@icloud.com>
2025-12-03 21:53:48 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-// src/routes/index.js
 const express = require('express');
 
 const vendasRoutes = require('./vendas.routes');
 const estoqueRoutes = require('./estoque.routes');
+const empresasRoutes = require('./empresas.routes');
 
 const router = express.Router();
 
@@ -16,5 +16,8 @@ router.use('/api/v1/vendas', vendasRoutes);
 
 // Estoque
 router.use('/api/v1/estoque', estoqueRoutes);
+
+// Empresas
+router.use('/api/v1/empresas', empresasRoutes);
 
 module.exports = router;
