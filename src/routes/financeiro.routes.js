4c23e243826a8d9976d4ac0dae3d33cfe2bcdb43
natanborges-diniz <natanborges@icloud.com>
2025-12-04 15:50:43 -0300

    Update financeiro.routes.js

--- src/routes/financeiro.routes.js
+++ src/routes/financeiro.routes.js
@@ -1,8 +1,9 @@
+// src/routes/financeiro.routes.js
 const express = require("express");
 const router = express.Router();
 const financeiroController = require("../controllers/financeiroController");
 
 // GET /api/v1/financeiro/parcelas
-router.get("/financeiro/parcelas", financeiroController.listarParcelas);
+router.get("/parcelas", financeiroController.listarParcelas);
 
 module.exports = router;
