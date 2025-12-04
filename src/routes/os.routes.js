326f01e034c9e844a3278d40b4427e0623d94db0
natanborges-diniz <natanborges@icloud.com>
2025-12-03 23:28:02 -0300

    Update os.routes.js

--- src/routes/os.routes.js
+++ src/routes/os.routes.js
@@ -1,10 +1,10 @@
 // src/routes/os.routes.js
 const express = require('express');
-const { monitorProducao } = require('../controllers/osController');
+const { monitorOS } = require('../controllers/osController');
 
 const router = express.Router();
 
-// GET /api/v1/os/monitor-producao
-router.get('/monitor-producao', monitorProducao);
+// GET /api/v1/os/monitor
+router.get('/monitor', monitorOS);
 
 module.exports = router;
