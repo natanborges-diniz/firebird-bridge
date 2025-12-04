4a11efac8ef514609304ad67dfaacfcf34842c1e
natanborges-diniz <natanborges@icloud.com>
2025-12-03 22:51:44 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,18 @@
+// src/routes/index.js
 const express = require('express');
 
 const vendasRoutes = require('./vendas.routes');
 const estoqueRoutes = require('./estoque.routes');
-const empresasRoutes = require('./empresas.routes');
+const osRoutes = require('./os.routes');
 
 const router = express.Router();
 
-// Health check simples
 router.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-// Vendas
 router.use('/api/v1/vendas', vendasRoutes);
-
-// Estoque
 router.use('/api/v1/estoque', estoqueRoutes);
-
-// Empresas
-router.use('/api/v1/empresas', empresasRoutes);
+router.use('/api/v1/os', osRoutes);
 
 module.exports = router;
