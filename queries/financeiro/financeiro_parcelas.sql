362a0d9e8c1624d4c597cfa025ef5988abc61c33
natanborges-diniz <natanborges@icloud.com>
2025-12-04 16:17:17 -0300

    Update financeiro_parcelas.sql

--- queries/financeiro/financeiro_parcelas.sql
+++ queries/financeiro/financeiro_parcelas.sql
@@ -1,10 +1,83 @@
 select
-  *
+  /* EMPRESA / CONTRAPARTE */
+  fl.cod_empresa                              as cod_empresa,
+  pe_emp.nome                                 as empresa_nome,
+  fl.cod_lancamento                           as cod_lancamento,
+  fl.pagar                                    as lancamento_pagar,      -- 'T' = PAGAR, 'F' = RECEBER
+  fl.previsao                                 as lancamento_previsao,   -- 'T' / 'F'
+  fl.numerodocumento                          as lancamento_documento,
+
+  p_cli.cod_pessoa                            as pessoa_cod_pessoa,
+  p_cli.nome                                  as pessoa_nome,
+
+  /* PARCELA */
+  fp.cod_lancamentoparcela                    as parcela_id,
+  fp.dataemissao                              as parcela_data_emissao,
+  fp.datavencimento                           as parcela_data_vencimento,
+  fp.datapagamento                            as parcela_data_pagamento,
+  fp.datarecebimento                          as parcela_data_recebimento,
+  fp.valor                                    as parcela_valor,
+  fp.valororiginal                            as parcela_valor_original,
+  fp.valorpago                                as parcela_valor_pago,
+
+  /* SITUAÇÃO CALCULADA SIMPLES */
+  cast(
+    case
+      when fp.datapagamento is not null then 'PAGA'
+      when fp.datapagamento is null
+           and fp.datavencimento < current_date then 'EM ATRASO'
+      else 'EM ABERTO'
+    end
+    as varchar(15)
+  )                                           as parcela_situacao,
+
+  /* CLASSIFICAÇÃO CONTÁBIL */
+  fcc.cod_contaclassificacao                  as contacla_codigo,
+  fcc.numeroconta                             as contacla_numero,
+  fcc.descricao                               as contacla_descricao,
+
+  /* FORMA DE PAGAMENTO (TIPO + DESCRIÇÃO) */
+  ffp.cod_formapagamento                      as formapagto_codigo,
+  ffp.cod_formapagamentotipo                  as formapagto_tipo_codigo,
+  ffp.descricao                               as formapagto_descricao,
+
+  cast(
+    case ffp.cod_formapagamentotipo
+      when 1 then 'DINHEIRO'
+      when 2 then 'CHEQUE'
+      when 3 then 'CARTÃO'
+      when 4 then 'BANCO'
+      when 5 then 'CARNÊ'
+      when 6 then 'CRÉDITO'
+      else '(NÃO DEFINIDA)'
+    end
+    as varchar(15)
+  )                                           as formapagto_tipo_nome
+
 from
-  VW_FIN_PARCELAS_COMPLETO
+  finlancamento fl
+    join finlancamentoparcela fp
+      on fp.cod_lancamento = fl.cod_lancamento
+
+    left join fincontaclassificacao fcc
+      on fcc.cod_contaclassificacao = fl.cod_contaclassificacao
+
+    left join finformapagamento ffp
+      on ffp.cod_formapagamento = fp.cod_formapagamento
+
+    /* Empresa do lançamento */
+    join pessoa pe_emp
+      on pe_emp.cod_pessoa = fl.cod_empresa
+
+    /* Pessoa cliente/fornecedor */
+    join pessoa p_cli
+      on p_cli.cod_pessoa = fl.cod_pessoa
+
 where
-  data_emissao between ? and ?
-  and cod_empresa = ?
+  fp.dataemissao between ? and ?
+  and fl.cod_empresa = ?
+
 order by
-  data_emissao,
-  cod_lancamento;
+  fp.dataemissao,
+  fl.cod_lancamento,
+  fp.cod_lancamentoparcela;
