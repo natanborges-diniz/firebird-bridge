eceb9d20e20519870084932fde133c23797a06d7
natanborges-diniz <natanborges@icloud.com>
2025-12-08 20:24:14 -0300

    Update financeiro_parcelas.sql

--- queries/financeiro/financeiro_parcelas.sql
+++ queries/financeiro/financeiro_parcelas.sql
@@ -1,16 +1,12 @@
-select
-  /* EMPRESA / CONTRAPARTE */
+select first 10
   fl.cod_empresa                              as cod_empresa,
   pe_emp.nome                                 as empresa_nome,
   fl.cod_lancamento                           as cod_lancamento,
-  fl.pagar                                    as lancamento_pagar,      -- 'T' = PAGAR, 'F' = RECEBER
-  fl.previsao                                 as lancamento_previsao,   -- 'T' / 'F'
+  fl.pagar                                    as lancamento_pagar,
+  fl.previsao                                 as lancamento_previsao,
   fl.numerodocumento                          as lancamento_documento,
-
   p_cli.cod_pessoa                            as pessoa_cod_pessoa,
   p_cli.nome                                  as pessoa_nome,
-
-  /* PARCELA */
   fp.cod_lancamentoparcela                    as parcela_id,
   fp.dataemissao                              as parcela_data_emissao,
   fp.datavencimento                           as parcela_data_vencimento,
@@ -19,24 +15,17 @@ select
   fp.valor                                    as parcela_valor,
   fp.valororiginal                            as parcela_valor_original,
   fp.valorpago                                as parcela_valor_pago,
-
-  /* SITUAÇÃO CALCULADA SIMPLES */
   case
     when fp.datapagamento is not null then 'PAGA'
     when fp.datapagamento is null
          and fp.datavencimento < current_date then 'EM ATRASO'
     else 'EM ABERTO'
   end                                           as parcela_situacao,
-
-  /* CLASSIFICAÇÃO CONTÁBIL */
   fcc.cod_contaclassificacao                  as contacla_codigo,
   fcc.numeroconta                             as contacla_numero,
   fcc.descricao                               as contacla_descricao,
-
-  /* FORMA DE PAGAMENTO (TIPO) */
   ffp.cod_formapagamento                      as formapagto_codigo,
   ffp.cod_formapagamentotipo                  as formapagto_tipo_codigo,
-
   case ffp.cod_formapagamentotipo
     when 1 then 'DINHEIRO'
     when 2 then 'CHEQUE'
@@ -46,30 +35,19 @@ select
     when 6 then 'CRÉDITO'
     else '(NÃO DEFINIDA)'
   end                                         as formapagto_tipo_nome
-
 from
   finlancamento fl
     join finlancamentoparcela fp
       on fp.cod_lancamento = fl.cod_lancamento
-
     left join fincontaclassificacao fcc
       on fcc.cod_contaclassificacao = fl.cod_contaclassificacao
-
     left join finformapagamento ffp
       on ffp.cod_formapagamento = fp.cod_formapagamento
-
-    /* Empresa do lançamento */
     join pessoa pe_emp
       on pe_emp.cod_pessoa = fl.cod_empresa
-
-    /* Pessoa cliente/fornecedor */
     join pessoa p_cli
       on p_cli.cod_pessoa = fl.cod_pessoa
-
-where
-  fl.cod_empresa = cast(? as integer)
-  and fp.datavencimento between cast(? as date) and cast(? as date)
-
+-- ⚠️ sem WHERE por enquanto
 order by
   fp.datavencimento,
   fl.cod_lancamento,
