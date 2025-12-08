98972dd031f5607c48da6f4674547ac1da8607e6
natanborges-diniz <natanborges@icloud.com>
2025-12-08 14:16:21 -0300

    Rename route and update controller method

--- src/routes/estoque.routes.js
+++ src/routes/estoque.routes.js
@@ -1,10 +1,8 @@
-// src/routes/estoque.routes.js
+// src/routes/estoqueRoutes.js
 const express = require('express');
-const { analiseEstoqueAcao } = require('../controllers/estoqueController');
-
 const router = express.Router();
+const estoqueController = require('../controllers/estoqueController');
 
-// GET /api/v1/estoque/analise-acao?codEmpresa=595
-router.get('/analise-acao', analiseEstoqueAcao);
+router.get('/analise', estoqueController.analiseEstoque);
 
 module.exports = router;
