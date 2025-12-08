e7ca2e2d66011ba9994d5f9d7a13edae04955f7f
natanborges-diniz <natanborges@icloud.com>
2025-12-08 14:17:58 -0300

    Add healthRoutes for health check endpoint

---
+++ src/routes/healthRoutes.js
@@ -0,0 +1,8 @@
+// src/routes/healthRoutes.js
+const express = require('express');
+const router = express.Router();
+const healthController = require('../controllers/healthController');
+
+router.get('/', healthController.health);
+
+module.exports = router;
