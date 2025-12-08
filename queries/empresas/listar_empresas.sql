a4146dad43cc26631228385621ca4a009e0ab2dd
natanborges-diniz <natanborges@icloud.com>
2025-12-08 14:37:00 -0300

    Replace query to list active companies with new format

--- queries/empresas/listar_empresas.sql
+++ queries/empresas/listar_empresas.sql
@@ -1,12 +1,6 @@
--- queries/empresas/listar_empresas.sql
--- Lista empresas (lojas) com codigo interno do ERP
-
-SELECT
-  EMPRESA.COD_EMPRESA,
-  PESSOA.NOME AS EMPRESA
-FROM
-  EMPRESA
-  JOIN PESSOA
-    ON PESSOA.COD_PESSOA = EMPRESA.COD_EMPRESA
-ORDER BY
-  PESSOA.NOME;
+SELECT 
+    COD_EMPRESA AS empresa,
+    NOME_FANTASIA AS nome
+FROM EMPRESAS
+WHERE ATIVA = 'S'
+ORDER BY NOME_FANTASIA;
