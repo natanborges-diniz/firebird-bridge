619baef717691ea0a099703dfa55e332df69a589
natanborges-diniz <natanborges@icloud.com>
2025-12-08 14:35:56 -0300

    Refactor empresaService to use updated SQL loading

--- src/services/empresaService.js
+++ src/services/empresaService.js
@@ -1,12 +1,12 @@
 // src/services/empresaService.js
-const { runQuery } = require('../db');
-const { loadSQL } = require('../utils/loadSQL');
 
-const listarEmpresasSql = loadSQL('empresas/listar_empresas.sql');
+const db = require('../db');
+const loadSQL = require('../utils/loadSQL');
 
 async function getEmpresas() {
-  const rows = await runQuery(listarEmpresasSql, []);
-  return rows;
+  const sql = loadSQL('empresas/listarEmpresas.sql');
+  const result = await db.runQuery(sql);
+  return result;
 }
 
 module.exports = {
