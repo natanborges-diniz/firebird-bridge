134a62f555c95e25c4540ef4c330d683ebdfc57d
natanborges-diniz <natanborges@icloud.com>
2025-12-09 10:14:27 -0300

    Update financeiro_parcelas.sql

--- queries/financeiro/financeiro_parcelas.sql
+++ queries/financeiro/financeiro_parcelas.sql
@@ -1,54 +1,11 @@
-select first 10
-  fl.cod_empresa                              as cod_empresa,
-  pe_emp.nome                                 as empresa_nome,
-  fl.cod_lancamento                           as cod_lancamento,
-  fl.pagar                                    as lancamento_pagar,
-  fl.previsao                                 as lancamento_previsao,
-  fl.numerodocumento                          as lancamento_documento,
-  p_cli.cod_pessoa                            as pessoa_cod_pessoa,
-  p_cli.nome                                  as pessoa_nome,
-  fp.cod_lancamentoparcela                    as parcela_id,
-  fp.dataemissao                              as parcela_data_emissao,
-  fp.datavencimento                           as parcela_data_vencimento,
-  fp.datapagamento                            as parcela_data_pagamento,
-  fp.datarecebimento                          as parcela_data_recebimento,
-  fp.valor                                    as parcela_valor,
-  fp.valororiginal                            as parcela_valor_original,
-  fp.valorpago                                as parcela_valor_pago,
-  case
-    when fp.datapagamento is not null then 'PAGA'
-    when fp.datapagamento is null
-         and fp.datavencimento < current_date then 'EM ATRASO'
-    else 'EM ABERTO'
-  end                                           as parcela_situacao,
-  fcc.cod_contaclassificacao                  as contacla_codigo,
-  fcc.numeroconta                             as contacla_numero,
-  fcc.descricao                               as contacla_descricao,
-  ffp.cod_formapagamento                      as formapagto_codigo,
-  ffp.cod_formapagamentotipo                  as formapagto_tipo_codigo,
-  case ffp.cod_formapagamentotipo
-    when 1 then 'DINHEIRO'
-    when 2 then 'CHEQUE'
-    when 3 then 'CARTÃO'
-    when 4 then 'BANCO'
-    when 5 then 'CARNÊ'
-    when 6 then 'CRÉDITO'
-    else '(NÃO DEFINIDA)'
-  end                                         as formapagto_tipo_nome
+select distinct
+  fl.cod_empresa as cod_empresa,
+  pe_emp.nome    as empresa_nome
 from
   finlancamento fl
     join finlancamentoparcela fp
       on fp.cod_lancamento = fl.cod_lancamento
-    left join fincontaclassificacao fcc
-      on fcc.cod_contaclassificacao = fl.cod_contaclassificacao
-    left join finformapagamento ffp
-      on ffp.cod_formapagamento = fp.cod_formapagamento
     join pessoa pe_emp
       on pe_emp.cod_pessoa = fl.cod_empresa
-    join pessoa p_cli
-      on p_cli.cod_pessoa = fl.cod_pessoa
--- ⚠️ sem WHERE por enquanto
-order by
-  fp.datavencimento,
-  fl.cod_lancamento,
-  fp.cod_lancamentoparcela;
+order by fl.cod_empresa
+rows 20;
