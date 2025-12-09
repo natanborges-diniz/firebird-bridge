30bb35ddde2dd96b011912838d507871ff5c2e6c
natanborges-diniz <natanborges@icloud.com>
2025-12-09 16:04:12 -0300

    Create debugController.js

---
+++ src/controllers/debugController.js
@@ -0,0 +1,20 @@
+// src/controllers/debugController.js
+const { success } = require('../utils/apiResponse');
+
+async function testarEmpresas(req, res) {
+  // Array "sujo": empresa normal, lixo e as duas SUPER
+  const linhas = [
+    { COD_EMPRESA: 1, EMPRESA: 'DINIZ PRIMITIVA I' },
+    { COD_EMPRESA: 3, EMPRESA: 'EMPRESA LIXO 3' },
+    { COD_EMPRESA: 13, EMPRESA: 'DINIZ SUPER (CNPJ antigo)' },
+    { COD_EMPRESA: 18, EMPRESA: 'DINIZ SUPER SHOPPING (CNPJ novo)' },
+    { cod_empresa: 5, empresa: 'EMPRESA LIXO 5' }
+  ];
+
+  // Vai passar pelo normalizeEmpresas dentro do success()
+  return success(res, linhas);
+}
+
+module.exports = {
+  testarEmpresas
+};
