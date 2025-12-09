d814a1f311eed337a7fd2ab435c0852ea8253777
natanborges-diniz <natanborges@icloud.com>
2025-12-09 11:34:28 -0300

    Update vendasRoutes.js

--- src/routes/vendasRoutes.js
+++ src/routes/vendasRoutes.js
@@ -1,10 +1,16 @@
 // src/routes/vendasRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const vendasController = require('../controllers/vendasController');
 
+// GET /vendas/resumo-empresa-vendedor
 router.get('/resumo-empresa-vendedor', vendasController.resumoEmpresaVendedor);
+
+// GET /vendas/resumo-formas-pagamento
 router.get('/resumo-formas-pagamento', vendasController.resumoFormasPagamento);
+
+// GET /vendas/analise-familia-vendedor
 router.get('/analise-familia-vendedor', vendasController.analiseFamiliaVendedor);
 
 module.exports = router;
