fa5c7b47a485860f2b1f1019cd77684d6a7693bb
natanborges-diniz <natanborges@icloud.com>
2025-12-15 13:07:39 -0300

    Update osRoutes.js

--- src/routes/osRoutes.js
+++ src/routes/osRoutes.js
@@ -1,10 +1,8 @@
-// src/routes/osRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const osController = require('../controllers/osController');
 
-// GET /os/monitor
 router.get('/monitor', osController.monitorOs);
+router.get('/monitor-ultima-etapa', osController.monitorOsUltimaEtapa);
 
 module.exports = router;
