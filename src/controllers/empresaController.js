fa3e6f6ffb478063f00f35d569dc2b75824ec09a
natanborges-diniz <natanborges@icloud.com>
2026-01-12 22:36:04 -0300

    Add caching options to listarEmpresas function

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -4,7 +4,10 @@ const { success, handleControllerError } = require("../utils/apiResponse");
 
 async function listarEmpresas(req, res) {
   try {
-    const empresas = await empresaService.getEmpresas();
+    const useCache = req.query.cache !== '0' && req.query.cache !== 'false';
+    const cacheTtlMs = req.query.cacheTtlMs ? Number(req.query.cacheTtlMs) : undefined;
+
+    const empresas = await empresaService.getEmpresas({ useCache, cacheTtlMs });
     return success(res, empresas);
   } catch (err) {
     return handleControllerError(res, err);
