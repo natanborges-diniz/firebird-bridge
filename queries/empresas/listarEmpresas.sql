ed9217444fdbebc46c095e8aa1bb96ee90f42839
natanborges-diniz <natanborges@icloud.com>
2026-01-20 19:54:20 -0300

    Update listarEmpresas.sql

--- queries/empresas/listarEmpresas.sql
+++ queries/empresas/listarEmpresas.sql
@@ -1,21 +1,11 @@
 -- queries/empresas/listarEmpresas.sql
--- Lista de empresas para o Lovable
+-- Lista de empresas conforme legado (sem unificar 13/18)
 -- Regras:
 --  - ignora empresas lixo: 3,5,7,8,11,12
---  - unifica SUPER (13 e 18) em uma empresa lógica só: "DINIZ SUPER"
 
 select distinct
-  -- código lógico (13 e 18 viram 13)
-  case 
-    when e.cod_empresa in (13, 18) then 13
-    else e.cod_empresa
-  end as cod_empresa,
-
-  -- nome lógico (SUPER unificada)
-  case 
-    when e.cod_empresa in (13, 18) then 'DINIZ SUPER'
-    else p.nome
-  end as empresa_nome
+  e.cod_empresa,
+  p.nome
 from
   empresa e
   join pessoa p
@@ -24,4 +14,4 @@ where
   -- remove empresas lixo
   e.cod_empresa not in (3, 5, 7, 8, 11, 12)
 order by
-  empresa_nome;
+  p.nome;
