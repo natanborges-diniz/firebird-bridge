e59445bc947d62964a2597b4917351145988a248
natanborges-diniz <natanborges@icloud.com>
2026-01-30 11:07:39 -0300

    Update analise_estoque_acao.sql

--- queries/estoque/analise_estoque_acao.sql
+++ queries/estoque/analise_estoque_acao.sql
@@ -1,6 +1,7 @@
 -- queries/estoque/analise_estoque_acao.sql
--- Análise de estoque completa (CAF, última entrada, média de venda, ação sugerida)
--- Filtro: empresa (pcod_empresa)
+-- Analise de estoque por empresa, fornecedor, grife, produto e acao sugerida.
+-- Parametros:
+--   1) cod_empresa (INTEGER)
 
 WITH
     tbEMPRESA AS (
@@ -53,7 +54,7 @@ WITH
             JOIN naturezaoperacao
               ON naturezaoperacao.cod_naturezaoperacao = transacao_item.cod_naturezaoperacao
         WHERE
-            naturezaoperacao.tipo = 2   -- entrada
+            naturezaoperacao.tipo = 2
         GROUP BY
             1, 2
     ),
@@ -75,111 +76,33 @@ WITH
              AND ordemcompra_item.cod_empresa = transacao_item.cod_empresa
              AND ordemcompra_item.cod_transacaoitem = transacao_item.cod_transacaoitem
         WHERE
-            transacao.situacao = 2   -- aprovadas
+            transacao.situacao = 2
         GROUP BY
             1
-    ),
-
-    tbMEDIAvenda AS (
-        SELECT
-            transacao_item.cod_item   AS cod_produto,
-            transacao_item.cod_empresa,
-
-            -- quantidades médias
-            SUM(
-                IIF(
-                    DATEDIFF(MONTH, transacao.dataencerramento, CURRENT_DATE) = 6,
-                    transacao_item.quantidade,
-                    NULL
-                )
-            ) / 1 AS qtd_1_mes,
-
-            SUM(
-                IIF(
-                    DATEDIFF(MONTH, transacao.dataencerramento, CURRENT_DATE) >= 4,
-                    transacao_item.quantidade,
-                    NULL
-                )
-            ) / 3 AS qtd_3_meses,
-
-            SUM(transacao_item.quantidade) / 6 AS qtd_6_meses,
-
-            -- giros médios
-            COUNT(
-                IIF(
-                    DATEDIFF(MONTH, transacao.dataencerramento, CURRENT_DATE) = 6,
-                    transacao.cod_transacao,
-                    NULL
-                )
-            ) / 1 AS giro_1_mes,
-
-            COUNT(
-                IIF(
-                    DATEDIFF(MONTH, transacao.dataencerramento, CURRENT_DATE) >= 4,
-                    transacao.cod_transacao,
-                    NULL
-                )
-            ) / 3 AS giro_3_meses
-
-        FROM
-            transacao
-            JOIN saida
-              ON saida.cod_empresa = transacao.cod_empresa
-             AND saida.cod_saida   = transacao.cod_transacao
-            JOIN venda
-              ON venda.cod_empresa = transacao.cod_empresa
-             AND venda.cod_venda   = transacao.cod_transacao
-            JOIN transacao_item
-              ON transacao_item.cod_empresa   = transacao.cod_empresa
-             AND transacao_item.cod_transacao = transacao.cod_transacao
-        WHERE
-            transacao.dataencerramento
-                BETWEEN DATEADD(-6 MONTH TO CURRENT_DATE) AND CURRENT_DATE
-        GROUP BY
-            1, 2
     )
 
 SELECT
-    -- empresa
-    tbEMPRESA.EMPRESA                                 AS empresa_nome,
-
-    -- fornecedor
-    pessoafornecedor.cod_pessoa                       AS fornecedor_cod_pessoa,
-    pessoafornecedor.nome                             AS fornecedor_nome,
-
-    -- grife / família
-    tbmarcamodeloar.descricao                         AS grife,
-
-    -- produto
-    produto.codigobarra                               AS codigo_barras,
-    item.descricao                                    AS descricao_item,
-
-    -- estoque / CAF
-    tbestoque.saldo                                   AS quantidade_estoque,
-    tbCAFproduto.CAF                                  AS caf,
-
-    -- datas e dias em estoque
-    tbDataUltimaentrada.data                          AS data_ultima_entrada,
-    DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-                                                      AS dias_estoque,
-
-    -- ação sugerida
+    tbEMPRESA.EMPRESA,
+    pessoafornecedor.cod_pessoa,
+    pessoafornecedor.nome AS nome_fornecedor,
+    tbmarcamodeloar.descricao AS grife,
+    produto.codigobarra AS codigo_barra,
+    item.descricao AS descricao_produto,
+    otiprodutoarmacao.tipo AS tipo_armacao,
+    tbestoque.saldo AS quantidade_estoque,
+    tbCAFproduto.CAF AS caf,
+    tbDataUltimaentrada.data AS data_ultima_entrada,
+    DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) AS dias_estoque,
     CASE
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             BETWEEN 0 AND 90   THEN 'ANÁLISE PARA RECOMPRA'
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             BETWEEN 91 AND 180 THEN 'ACOMPANHAMENTO'
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             BETWEEN 181 AND 270 THEN 'SINAL DE ALERTA'
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             BETWEEN 271 AND 360 THEN 'LIQUIDA 20%'
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             BETWEEN 361 AND 720 THEN 'LIQUIDA 30%'
-        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE)
-             > 720              THEN 'LIQUIDA 50%'
+        WHEN tbDataUltimaentrada.data IS NULL THEN 'SEM MOVIMENTO'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) BETWEEN 0 AND 90 THEN 'ANALISE PARA RECOMPRA'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) BETWEEN 91 AND 180 THEN 'ACOMPANHAMENTO'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) BETWEEN 181 AND 270 THEN 'SINAL DE ALERTA'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) BETWEEN 271 AND 360 THEN 'LIQUIDA 20%'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) BETWEEN 361 AND 720 THEN 'LIQUIDA 30%'
+        WHEN DATEDIFF(DAY FROM tbDataUltimaentrada.data TO CURRENT_DATE) > 720 THEN 'LIQUIDA 50%'
         ELSE 'DADOS INSUFICIENTES'
-    END                                               AS acao_sugerida
-
+    END AS acao_sugerida
 FROM
     item
     JOIN produto
@@ -201,10 +124,6 @@ FROM
      AND tbDataUltimaentrada.cod_empresa = tbestoque.cod_empresa
     LEFT JOIN tbCAFproduto
       ON tbCAFproduto.cod_produto = produto.cod_produto
-    LEFT JOIN tbMEDIAvenda
-      ON tbMEDIAvenda.cod_produto = produto.cod_produto
-     AND tbMEDIAvenda.cod_empresa = tbestoque.cod_empresa
-
 WHERE
-    tbestoque.cod_empresa = CAST(? AS INTEGER)
+    tbestoque.cod_empresa = ?  -- cod_empresa (parametro)
 ;
