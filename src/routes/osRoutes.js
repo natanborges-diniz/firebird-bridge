d654957f769d3edcd41d2c1a2fcba9fbd95fd410
natanborges-diniz <natanborges@icloud.com>
2026-02-05 12:25:28 -0300

    Update osRoutes.js

--- src/routes/osRoutes.js
+++ src/routes/osRoutes.js
@@ -4,5 +4,8 @@ const osController = require('../controllers/osController');
 
 router.get('/monitor', osController.monitorOs);
 router.get('/monitor-ultima-etapa', osController.monitorOsUltimaEtapa);
+router.get('/receitas-metadata', osController.receitaMetadata);
+router.get('/receita-metadata', osController.receitaMetadata);
+router.get('/receitas/metadata', osController.receitaMetadata);
 
 module.exports = router;
