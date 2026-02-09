7cec4e9b22ba73df0d658fda56472ee81753654f
natanborges-diniz <natanborges@icloud.com>
2026-02-09 14:29:43 -0300

    Change ORDER BY clause in hub_receitas.sql

--- queries/os/hub_receitas.sql
+++ queries/os/hub_receitas.sql
@@ -18,7 +18,7 @@ WITH itens_lente AS (
             i.descricao,
             ROW_NUMBER() OVER (
                 PARTITION BY ti.cod_ordemservicocaixa 
-                ORDER BY COALESCE(ti.seq_item, 999999), ti.cod_transacao_item
+                ORDER BY ti.cod_transacao_item
             ) AS rn
         FROM transacao_item ti
         JOIN item i ON i.cod_item = ti.cod_item
